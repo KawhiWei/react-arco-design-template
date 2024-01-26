35844b80a17720e7986a8c5fbefc1cdfd797a2b5
王泽威 <1585955375@qq.com>
2024-01-26 16:08:22 +0800

    feature : 修改login组件的加载方式

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -7,40 +7,44 @@ import {
     MenuComponent,
     NavBarItemComponent
 } from './components';
+import React, { Suspense } from 'react';
 
 import { Layout } from '@arco-design/web-react';
+import LoadingComponent from '@/components/loading';
 import { Outlet } from 'react-router-dom';
-import React from 'react';
 
+const load = (children: any) => <Suspense fallback={<LoadingComponent />}>{children}</Suspense>;
 const { Sider } = Layout;
 const { Header } = Layout;
 const { Footer } = Layout;
 const { Content } = Layout;
 const PublicLayout = () => {
     return (
-        <Layout className="layout-container">
-            <Header className="layout-header">
-                <LogoComponent />
-                <div className="layout-header-right">
-                    <NavBarItemComponent />
-                    <AvatarComponent />
-                </div>
-            </Header>
-            <Layout className="layout-main-wrap">
-                <Sider width="240" breakpoint="xl">
-                    <MenuComponent />
-                </Sider>
-                <Content className="layout-content">
-                    <div className="layout-content-breadcrumb">
-                        <BreadcrumbComponent />
-                        <div className="layout-main-content">
-                            <Outlet />
-                        </div>
+        <div>
+            {load(<Layout className="layout-container">
+                <Header className="layout-header">
+                    <LogoComponent />
+                    <div className="layout-header-right">
+                        <NavBarItemComponent />
+                        <AvatarComponent />
                     </div>
-                    <Footer className="layout-footer">© hu-snail-2022 arco-admin-template</Footer>
-                </Content>
-            </Layout>
-        </Layout>
+                </Header>
+                <Layout className="layout-main-wrap">
+                    <Sider width="240" breakpoint="xl">
+                        <MenuComponent />
+                    </Sider>
+                    <Content className="layout-content">
+                        <div className="layout-content-breadcrumb">
+                            <BreadcrumbComponent />
+                            <div className="layout-main-content">
+                                <Outlet />
+                            </div>
+                        </div>
+                        <Footer className="layout-footer">© hu-snail-2022 arco-admin-template</Footer>
+                    </Content>
+                </Layout>
+            </Layout>)}
+        </div>
     )
 };
 export default React.memo(PublicLayout);
